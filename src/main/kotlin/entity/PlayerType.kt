da5bdce1022f38da1b54f2e264b44503aa6e2787
oezdem05 <83602804+oezdem05@users.noreply.github.com>
2022-12-12 19:35:13 +0100

    Finished the enum class PlayerType

---
+++ src/main/kotlin/entity/PlayerType.kt
@@ -0,0 +1,23 @@
+package entity
+
+/**
+ * Enum to distinguish between the for [PlayerType]s:
+ * AI easy, AI hard, human and network
+ */
+enum class PlayerType {
+    AI_EASY,
+    AI_HARD,
+    HUMAN,
+    NETWORK,
+    ;
+
+    /**
+     * provide a string to represent the [PlayerType] of the player
+     */
+    override fun toString() = when(this) {
+        AI_EASY -> "AI_easy"
+        AI_HARD -> "AI_hard"
+        HUMAN -> "human"
+        NETWORK -> "network"
+    }
+}
