a4fc5ef558203c84e05ef514ffa078749d6e2e8c
oezdem05 <83602804+oezdem05@users.noreply.github.com>
2022-12-12 19:45:13 +0100

    Finished tests for the PlayerInfo class

---
+++ src/test/kotlin/service/PlayerInfoTest.kt
@@ -0,0 +1,31 @@
+package service
+
+import entity.Color
+import entity.PlayerInfo
+import entity.PlayerType
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+/**
+ * This class is used to test the Entity class [PlayerInfo]
+ */
+class PlayerInfoTest {
+
+    /**
+     * @property player1 is used as property to test if the entitylayer is working properly for the PlayerInfo
+     */
+    var player1 : PlayerInfo = PlayerInfo()
+
+    /**
+     * First we assign some values for player1 and check if they have been assigned properly.
+     */
+    @Test
+    fun constructorTest() {
+        player1.name = "TestName"
+        player1.color = Color.YELLOW
+        player1.playerType = PlayerType.HUMAN
+        assertEquals("TestName", player1.name)
+        assertEquals(Color.YELLOW, player1.color)
+        assertEquals(PlayerType.HUMAN, player1.playerType)
+    }
+}
