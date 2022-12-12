a85101ad5b2c81bfe9ab3ce5af5cdb1c3a064f89
oezdem05 <83602804+oezdem05@users.noreply.github.com>
2022-12-12 19:06:43 +0100

    Created PlayerInfo class

---
+++ src/main/kotlin/entity/PlayerInfo.kt
@@ -0,0 +1,12 @@
+package entity
+
+/**
+ * This class contains the information of the Playerobjects.
+ */
+class PlayerInfo {
+
+    /**
+     * @property name of the Player
+     */
+    var name : String? = null
+}
