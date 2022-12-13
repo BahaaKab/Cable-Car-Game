29383f89798651c45b1924f6706b055aadf51413
oezdem05 <83602804+oezdem05@users.noreply.github.com>
2022-12-13 14:36:48 +0100

    Updated PlayerInfo

--- src/main/kotlin/entity/PlayerInfo.kt
+++ src/main/kotlin/entity/PlayerInfo.kt
@@ -2,25 +2,21 @@ package entity
 
 /**
  * This class contains the information of the Playerobjects.
+ *
+ * @property name of the Player
+ *
+ * @property playerType is either AI (easy or hard), HUMAN or NETWORK
+ * While an easy AI player plays by doing random moves, the hard AI actually tries to win.
+ * A Human player  is a player using this computer while a NETWORK player is someone who joines in our session
+ * over the networks.
+ *
+ * @property color of the Player
  */
-class PlayerInfo {
+data class PlayerInfo(
+    val name: String,
+    var playerType: PlayerType,
+    var color: Color
+) {
 
-    /**
-     * @property name of the Player
-     */
-    public var name : String? = null
-
-    /**
-     * @property playerType is either AI (easy or hard), HUMAN or NETWORK
-     * While an easy AI player plays by doing random moves, the hard AI actually tries to win.
-     * A Human player  is a player using this computer while a NETWORK player is someone who joines in our session
-     * over the networks.
-     */
-    public lateinit var playerType : PlayerType
-
-    /**
-     * @property color of the Player
-     */
-    public lateinit var color : Color
 
 }
