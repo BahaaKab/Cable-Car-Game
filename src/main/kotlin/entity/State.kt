40b1677585c7208c6a49f0fc1299d0ef52df7938
Mert Can Özdemir <mertcan.oezdemir@tu-dortmund.de>
2022-12-14 10:59:21 +0100

    Updated the naming of the drawPile

--- src/main/kotlin/entity/State.kt
+++ src/main/kotlin/entity/State.kt
@@ -3,10 +3,10 @@ package entity
 /**
  * The [State] Class is used to save the current state of the game-board.
  *
- * @property drawpile is the current stack of GameTiles. It contains between 0 and 60 GameTiles
+ * @property drawPile is the current stack of GameTiles. It contains between 0 and 60 GameTiles
  * @property activePlayer is used to indicate the current Player
  */
-class State (var drawpile : MutableList<GameTile>, var activePlayer : Player) {
+class State (var drawPile : MutableList<GameTile>, var activePlayer : Player) {
 
     /**
      * @property board is a two-dimensional array which represents the game-board
