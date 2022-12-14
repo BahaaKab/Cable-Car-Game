82043ee741a4e19f5ab206ac259f2ece7d5659b5
oezdem05 <83602804+oezdem05@users.noreply.github.com>
2022-12-14 11:08:25 +0100

    added the board to the constructor

--- src/main/kotlin/entity/State.kt
+++ src/main/kotlin/entity/State.kt
@@ -5,12 +5,8 @@ package entity
  *
  * @property drawPile is the current stack of GameTiles. It contains between 0 and 60 GameTiles
  * @property activePlayer is used to indicate the current Player
+ * @property board is a two-dimensional array which represents the game-board
  */
-class State (var drawPile : MutableList<GameTile>, var activePlayer : Player) {
-
-    /**
-     * @property board is a two-dimensional array which represents the game-board
-     */
-    var board: Array<Array<Tile?>> = arrayOf()
+class State (var drawPile : MutableList<GameTile>, var activePlayer : Player, val board: Array<Array<Tile?>> = arrayOf()) {
 
 }
