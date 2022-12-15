e7391bc5cc5f4655838e22415db6e9e793973e5a
lucas.wilde <lucas-wilde@t-online.de>
2022-12-15 15:45:53 +0100

    Updated Player + Deep-Copy-Function

--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -5,8 +5,27 @@ package entity
  * @property color
  * @property name
  * @property score
+ * @property handTile
+ * @property currentTile
  */
-class Player(val playerType : PlayerType, val color : Color, val name : String,
+data class Player(val playerType : PlayerType, val color : Color, val name : String,
             val stationTiles : List<StationTile>) {
+
     var score : Int = 0
+    var handTile : GameTile? = null
+    var currentTile : GameTile? = null
+
+    // Deep-Copy-Function
+    fun deepCopy() : Player {
+        // Deep-Copy for stationTiles
+        val stationTiles : List<StationTile> = this.stationTiles.map {
+           return it.deepCopy()
+        }
+        // Deep-Copy for Player
+        return Player(playerType, color, name, stationTiles).also {
+            it.score = score
+            it.handTile = handTile.deepCopy()
+            it.currentTile = currentTile.deepCopy()
+        }
+    }
 }
