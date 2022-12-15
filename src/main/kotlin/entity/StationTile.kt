77230c1b5190c512cd87a7cce18055f3c7562c4a
philip <popien.philip@gmail.com>
2022-12-15 12:54:39 +0100

    Fix start position requirements.

--- src/main/kotlin/entity/StationTile.kt
+++ src/main/kotlin/entity/StationTile.kt
@@ -5,23 +5,23 @@ package entity
  * Implements a [Tile] with two [Tile.connectors] and no [Tile.connections]. The connectors have to be on the same side
  * of the tile.
  *
- * @property stationPosition The position where a cable car is set at the start of the game.
+ * @property startPosition The position where a cable car is set at the start of the game.
  *
- * @throws IllegalArgumentException If the wrong amount of connections or connectors is set or if no [stationPosition]
+ * @throws IllegalArgumentException If the wrong amount of connections or connectors is set or if no [startPosition]
  * is found.
  * @constructor Creates a station tile.
  */
 class StationTile(connectors: List<Position>): Tile(connectors) {
     override val isEmpty = false
     override val isEndTile = true
-    val stationPosition: Position
+    val startPosition: Position
 
     init {
         require(connectors.size == 2)
         require(connections.isEmpty())
-        // If the connectors were set correctly, the start position is always the even of the two (either 0, 2, 4 or 6)
-        stationPosition = requireNotNull(
-            connectors.find { it.value % 2 == 0 }
+        // If the connectors were set correctly, the start position is always the odd of the two (either 1, 3, 5 or 7)
+        startPosition = requireNotNull(
+            connectors.find { it.value % 2 == 1 }
         )
     }
 }
