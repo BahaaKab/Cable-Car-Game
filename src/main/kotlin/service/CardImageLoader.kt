0ce3e97155b1057fa0fc965db98dc12dd78a67a2
fabian.timmermann <fabian.timmermann@tu-dortmund.de>
2022-12-16 13:29:43 +0100

    Delete CardImageLoader and CardImageLoaderTest. We don't need them anymore.

--- src/main/kotlin/service/CardImageLoader.kt
+++ src/main/kotlin/service/CardImageLoader.kt
@@ -8,57 +8,6 @@ private const val IMG_HEIGHT = 200
 private const val IMG_WIDTH = 130
 
 /**
- * Provides access to the src/main/resources/card_deck.png file that contains all card images
- * in a raster. The returned [BufferedImage] objects of [frontImageFor], [blankImage],
- * and [backImage] are 130x200 pixels.
- */
-class CardImageLoader {
-
-    /**
-     * The full raster image containing the suits as rows (plus one special row for blank/back)
-     * and values as columns (starting with the ace). As the ordering does not correctly reflect
-     * the order in which the suits are declared in [CardSuit], mappings via [row] and [column]
-     * are required.
-     */
-    //private val image : BufferedImage = ImageIO.read(CardImageLoader::class.java.getResource(CARDS_FILE))
-
-    /**
-     * Provides the card image for the given [CardSuit] and [CardValue]
-     */
-    fun frontImageFor() {
-    }
-
-
-    /**
-     * Provides a blank (white) card
-     */
-    //val blankImage : BufferedImage get() = getImageByCoordinates(0, 4)
-
-    /**
-     * Provides the back side image of the card deck
-     */
-    // backImage: BufferedImage get() = getImageByCoordinates(2, 4)
-
-    /**
-     * retrieves from the full raster image [image] the corresponding sub-image
-     * for the given column [x] and row [y]
-     *
-     * @param x column in the raster image, starting at 0
-     * @param y row in the raster image, starting at 0
-     *
-     */
-    private fun getImageByCoordinates (x: Int, y: Int){} /*: BufferedImage =
-        image.getSubimage(
-            x * IMG_WIDTH,
-            y * IMG_HEIGHT,
-            IMG_WIDTH,
-            IMG_HEIGHT
-        )
-        */
-
-}
-
-/**
  * As the [CARDS_FILE] does not have the same ordering of suits
  * as they are in [CardSuit], this extension property provides
  * a corresponding mapping to be used when addressing the row.
