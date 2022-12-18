e10ff23c09e2bf7bfe527f13fb83ee238b263565
philip <popien.philip@gmail.com>
2022-12-18 20:40:32 +0100

    Create state test

--- src/test/kotlin/entity/StateTest.kt
+++ src/test/kotlin/entity/StateTest.kt
@@ -1,9 +1,32 @@
 package entity
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
+/**
+ * Tests for the [State] class.
+ */
 class StateTest {
+    /**
+     * Test the constructor of the [State] class.
+     */
     @Test
     fun testConstructor() {
+        val drawPile = List(10) { index ->
+            GameTile(index, List(8) { it }.shuffled())
+        }.toMutableList()
+
+        val board = Array<Array<Tile?>>(10) {
+            Array(10) { null }
+        }
+
+        val players = List(6) { Player(PlayerType.HUMAN, Color.BLUE, it.toString(), listOf()) }
+        val activePlayer = players[0]
+        val state = State(drawPile, activePlayer, board, players)
+
+        assertEquals(drawPile, state.drawPile)
+        assertEquals(board, state.board)
+        assertEquals(players, state.players)
+        assertEquals(activePlayer, state.activePlayer)
     }
 }
