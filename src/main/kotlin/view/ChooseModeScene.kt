47cf7c215c57052a328c9aa683812da13c62c7b3
fabian.timmermann <fabian.timmermann@tu-dortmund.de>
2022-12-21 22:54:40 +0100

    Editing the colors

--- src/main/kotlin/view/ChooseModeScene.kt
+++ src/main/kotlin/view/ChooseModeScene.kt
@@ -8,27 +8,31 @@ import tools.aqua.bgw.core.Alignment
 import tools.aqua.bgw.util.Font
 import tools.aqua.bgw.visual.ColorVisual
 import java.awt.Color
-import javafx.*
 
 @Suppress("UNUSED_PARAMETER","UNUSED","UndocumentedPublicFunction","UndocumentedPublicClass","EmptyFunctionBlock")
 
 
 class ChooseModeScene(private val rootService: RootService) : MenuScene(1920, 1080), Refreshable {
 
+    // Farbe London Underground/tfl: 36, 53, 136,1
+    // Farbe unsre GUI-Vorlage: 5, 24, 156, 1
+
     private val cableCarLabel : Label = Label(
         posX = 640, posY = 40,
         width = 640, height = 170,
         text = "CABLECAR",
-        font = Font(size = 100, color = Color.WHITE),
+        font = Font(size = 100, color = Color.WHITE, fontWeight = Font.FontWeight.BOLD), //, family = "sans-serif" ),
         alignment = Alignment.CENTER,
-        visual = ColorVisual(14, 40, 130)
-    )
+        visual = ColorVisual(5,24,156)
+    ).apply { componentStyle = "-fx-font-family: sans-serif" }
+
+    // Arial == sans-serif; cursive (über fx-font == Johnston == Times == Helvetica == NameXY???
 
     private val undergroundLabel : Label = Label(
         posX = 660, posY= 205,
         width = 600, height = 80,
         text = "UNDERGROUND",
-        font = Font(size = 60, color = Color(14, 40, 130)),
+        font = Font(size = 60, color = Color(5,24,156)),
         alignment = Alignment.CENTER
     )
 
@@ -36,7 +40,7 @@ class ChooseModeScene(private val rootService: RootService) : MenuScene(1920, 10
         posX = 560, posY = 380,
         width = 800, height = 140,
         text = "Which type of Game would you like to play?",
-        font = Font(size = 20, color = Color(14, 40, 130)),
+        font = Font(size = 20, color = Color(5,24,156), /* fontWeight = Font.FontWeight.SEMI_BOLD*/),
         alignment = Alignment.CENTER
     ).apply { componentStyle = ("-fx-background-color: rgba(255,255,255,1);-fx-background-radius: 30;" +
             "-fx-effect: dropshadow(one-pass-box, rgba(0,0,0,0.2), 10, 0, -1, 2);") }
@@ -51,7 +55,7 @@ class ChooseModeScene(private val rootService: RootService) : MenuScene(1920, 10
         font = Font(size = 23, color = Color.WHITE),
         alignment = Alignment.CENTER,
         visual = ColorVisual(249, 249, 250)
-    ).apply { componentStyle = "-fx-background-color: rgba(14,40,130,1);-fx-background-radius: 100" }
+    ).apply { componentStyle = "-fx-background-color: rgba(5,24,156,1);-fx-background-radius: 100" }
 
     private val networkButton : Button = Button(
         posX = 860, posY = 680,
@@ -60,12 +64,12 @@ class ChooseModeScene(private val rootService: RootService) : MenuScene(1920, 10
         font = Font(size = 23, color = Color.WHITE),
         alignment = Alignment.CENTER,
         visual = ColorVisual(249, 249, 250)
-    ).apply { componentStyle = "-fx-background-color: rgba(14,40,130,1);-fx-background-radius: 100" }
+    ).apply { componentStyle = "-fx-background-color: rgba(5,24,156,1);-fx-background-radius: 100" }
 
 // Beispiel für Hintergrundfarbe: "-fx-background-color: rgba(150,40,173,1);
     init {
         opacity = 1.0
-        background = ColorVisual(249, 249, 250)
+        background = ColorVisual(247, 247, 247)
         addComponents(cableCarLabel, undergroundLabel, chooseModeLabel)
         addComponents(localButton, networkButton)
     }
