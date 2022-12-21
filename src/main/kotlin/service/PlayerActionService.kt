b6e26c3c2aa134ac51be40fd9d9e34555d426fa0
oezdem05 <83602804+oezdem05@users.noreply.github.com>
2022-12-21 18:39:04 +0100

    Created undo function
    updates #31

--- src/main/kotlin/service/PlayerActionService.kt
+++ src/main/kotlin/service/PlayerActionService.kt
@@ -1,10 +1,22 @@
 package service
 
+import entity.*
+
 @Suppress("UNUSED_PARAMETER","UNUSED","UndocumentedPublicFunction","UndocumentedPublicClass","EmptyFunctionBlock")
 
 class PlayerActionService(private val rootService: RootService) : AbstractRefreshingService() {
 
+    /**
+     * Undoes the last game [State] and moves on to the nextTurn.
+     * **/
     fun undo() {
+        //Create a local variable to refer to the History object
+        val gameHistory = rootService.cableCar!!.history
+        //get some undo-magic done
+        val undo : State = gameHistory.undoStates.pop()
+        gameHistory.redoStates.add(undo)
+        //Move on to the next turn
+        rootService.cableCarService.nextTurn()
     }
 
     fun redo() {
