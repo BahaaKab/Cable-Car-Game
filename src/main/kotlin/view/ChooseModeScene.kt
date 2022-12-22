566b0c3cb2f5c85e1bd960a4e80716964593e8c2
fabian.timmermann <fabian.timmermann@tu-dortmund.de>
2022-12-22 23:43:42 +0100

    Editing Fonts of Labels and Buttons

--- src/main/kotlin/view/ChooseModeScene.kt
+++ src/main/kotlin/view/ChooseModeScene.kt
@@ -15,24 +15,24 @@ import java.awt.Color
 class ChooseModeScene(private val rootService: RootService) : MenuScene(1920, 1080), Refreshable {
 
     // Farbe London Underground/tfl: 36, 53, 136,1
-    // Farbe unsre GUI-Vorlage: 5, 24, 156, 1
+    // Farbe unsere GUI-Vorlage: 5, 24, 156, 1
 
     private val cableCarLabel : Label = Label(
         posX = 640, posY = 40,
         width = 640, height = 170,
         text = "CABLECAR",
-        font = Font(size = 100, color = Color.WHITE, fontWeight = Font.FontWeight.BOLD), //, family = "sans-serif" ),
+        font = Font(size = 120, color = Color.WHITE,
+            fontWeight = Font.FontWeight.BOLD, family = "Johnston ITC Std Light" ),
         alignment = Alignment.CENTER,
         visual = ColorVisual(5,24,156)
-    ).apply { componentStyle = "-fx-font-family: sans-serif" }
-
-    // Arial == sans-serif; cursive (über fx-font == Johnston == Times == Helvetica == NameXY???
+    )
 
     private val undergroundLabel : Label = Label(
-        posX = 660, posY= 205,
+        posX = 660, posY= 208,
         width = 600, height = 80,
         text = "UNDERGROUND",
-        font = Font(size = 60, color = Color(5,24,156)),
+        font = Font(size = 70, color = Color(5,24,156),
+            fontWeight = Font.FontWeight.BOLD, family = "Johnston ITC Std Light"),
         alignment = Alignment.CENTER
     )
 
@@ -40,19 +40,17 @@ class ChooseModeScene(private val rootService: RootService) : MenuScene(1920, 10
         posX = 560, posY = 380,
         width = 800, height = 140,
         text = "Which type of Game would you like to play?",
-        font = Font(size = 20, color = Color(5,24,156), /* fontWeight = Font.FontWeight.SEMI_BOLD*/),
+        font = Font(size = 20, color = Color(5,24,156)
+            , fontWeight = Font.FontWeight.SEMI_BOLD, family = "Johnston ITC Std Medium"),
         alignment = Alignment.CENTER
-    ).apply { componentStyle = ("-fx-background-color: rgba(255,255,255,1);-fx-background-radius: 30;" +
+    ).apply { componentStyle = ("-fx-background-color: rgba(255,255,255,1);-fx-background-radius: 30;"+
             "-fx-effect: dropshadow(one-pass-box, rgba(0,0,0,0.2), 10, 0, -1, 2);") }
-
-
-    //.apply { componentStyle = "-fx-effect: dropshadow( gaussian , rgba(0,0,0,0.2) , 10,0,-2,4 );" }
 
     private val localButton : Button = Button(
         posX = 860, posY = 580,
         width = 200, height = 50,
         text = "Local Game",
-        font = Font(size = 23, color = Color.WHITE),
+        font = Font(size = 23, color = Color.WHITE, family = "Johnston ITC Std Light"),
         alignment = Alignment.CENTER,
         visual = ColorVisual(249, 249, 250)
     ).apply { componentStyle = "-fx-background-color: rgba(5,24,156,1);-fx-background-radius: 100" }
@@ -61,7 +59,7 @@ class ChooseModeScene(private val rootService: RootService) : MenuScene(1920, 10
         posX = 860, posY = 680,
         width = 200, height = 50,
         text = "Network Game",
-        font = Font(size = 23, color = Color.WHITE),
+        font = Font(size = 23, color = Color.WHITE, family = "Johnston ITC Std Light"),
         alignment = Alignment.CENTER,
         visual = ColorVisual(249, 249, 250)
     ).apply { componentStyle = "-fx-background-color: rgba(5,24,156,1);-fx-background-radius: 100" }
