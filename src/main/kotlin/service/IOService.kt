8b00e66bc0a5dc67310af61291388e5ebf155483
oezdem05 <83602804+oezdem05@users.noreply.github.com>
2022-12-26 14:14:48 +0100

    updated the initializeDrawPile Method
    updates #44

--- src/main/kotlin/service/IOService.kt
+++ src/main/kotlin/service/IOService.kt
@@ -1,8 +1,15 @@
 package service
+
+import entity.GameTile
+
 @Suppress("UNUSED_PARAMETER","UNUSED","UndocumentedPublicFunction","UndocumentedPublicClass","EmptyFunctionBlock")
 
 class IOService {
     private val rootService: RootService? = null
     fun readFromCSV() {
+    }
+
+    fun getTilesFromCSV() : List<GameTile>{
+        return emptyList()
     }
 }
--- src/main/kotlin/service/SetupService.kt
+++ src/main/kotlin/service/SetupService.kt
@@ -2,6 +2,8 @@ package service
 
 import entity.PlayerInfo
 import entity.GameTile
+import entity.PlayerType
+import kotlin.random.Random
 
 @Suppress("UNUSED_PARAMETER","UNUSED","UndocumentedPublicFunction","UndocumentedPublicClass","EmptyFunctionBlock")
 class SetupService(private val rootService: RootService) : AbstractRefreshingService() {
@@ -18,7 +20,29 @@ class SetupService(private val rootService: RootService) : AbstractRefreshingSer
     }
 
     private fun createPlayers(playerInfos: List<PlayerInfo>) {}
+
+    /**
+     * Returns the drawPile.
+     * If played locally we just return the drawPile.
+     * If Played over the network we import the order of the drawPile from the GameInitMessage
+     * **/
     private fun initializeDrawPile(): List<GameTile>? {
-        return null
+        var online = false
+        val players = rootService.cableCar!!.currentState!!.players
+        val game = rootService.ioService
+        for(i in players.indices){
+            if (players[i].playerType == PlayerType.NETWORK) online = true
+            break
+        }
+        if (!online){
+            //local game just returns a list
+            val gameTiles = game!!.getTilesFromCSV()
+            return gameTiles
+        } else {
+            //Some Network magic needs to happen here...
+            //Keine Ahnung wie die GameInitMessage funktioniert
+            return null
+        }
     }
+
 }
