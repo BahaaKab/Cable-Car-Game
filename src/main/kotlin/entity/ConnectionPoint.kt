037ab2c8afd7d7df99da0d04b46cf015abbeb775
lucas.wilde <lucas-wilde@t-online.de>
2022-12-27 12:48:21 +0100

    Deleted unused Entity-classes

--- src/main/kotlin/entity/ConnectionPoint.kt
+++
@@ -1,7 +0,0 @@
-package entity
-
-/** A ConnectionPoint represents the mapping of a Position on the Tile to a ConnectionPoint of the Tile.
- *
- * @param position represents the visual position on a Tile.*/
-class ConnectionPoint(var position : Position) {
-}
