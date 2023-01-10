e79f6d7546ce54ccf79538346649c38a7dbaf363
Erik Schmidt <erik.schmidt@tu-dortmund.de>
2023-01-10 19:11:33 +0100

    Added visuals

--- src/main/kotlin/view/Constants.kt
+++ src/main/kotlin/view/Constants.kt
@@ -8,6 +8,7 @@ const val DEFAULT_FONT_BOLD = "Johnston ITC Std Light" // It says light but is i
 const val DEFAULT_FONT_MEDIUM = "Johnston ITC Std Medium"
 val DEFAULT_BLUE = Color(5, 24, 156)
 val DEFAULT_BLUE_VISUAL = ColorVisual(5, 24, 156)
+const val DEFAULT_BLUE_STRING = "5,24,156"
 const val DEFAULT_GREY_STRING = "233,233,236"
 val DEFAULT_YELLOW_COLOR = ColorVisual(253,211,41)
 val DEFAULT_BLUE_COLOR = ColorVisual(17,139,206)
--- src/main/kotlin/view/EndScene.kt
+++ src/main/kotlin/view/EndScene.kt
@@ -2,8 +2,56 @@ package view
 
 import tools.aqua.bgw.core.MenuScene
 import service.RootService
-@Suppress("UNUSED_PARAMETER","UNUSED","UndocumentedPublicFunction","UndocumentedPublicClass","EmptyFunctionBlock")
+import tools.aqua.bgw.components.uicomponents.Button
+import tools.aqua.bgw.core.Alignment
+import tools.aqua.bgw.util.Font
+import tools.aqua.bgw.visual.ColorVisual
+import tools.aqua.bgw.visual.Visual
+import view.components.CableCarLogo
+import view.components.ScoreboardPane
+import java.awt.Color
 
-class EndScene(private val rootService: RootService) : MenuScene(), Refreshable {
+/**
+ * This class manages the end scene of the application. Here the players see their final scores and have
+ * the option to exit the game or go back to the main menu.
+ *
+ * @param rootService The administration class for the entity and service layer.
+ */
+class EndScene(private val rootService: RootService) : MenuScene(1920,1080), Refreshable {
 
+    private val logoPane = CableCarLogo(posX = 817, posY = 104).apply {
+        scale = 1.2
+    }
+
+    private val scoreboardPane = ScoreboardPane(775, 300)
+
+    val exitButton = Button(
+        posX = 870, posY = 775,
+        width = 180, height = 50,
+        text = "Exit",
+        font = Font(size = 25, color = Color.WHITE, family = DEFAULT_FONT_MEDIUM),
+        alignment = Alignment.CENTER,
+        visual = Visual.EMPTY
+    ).apply {
+        componentStyle = "-fx-background-color: rgb($DEFAULT_BLUE_STRING);-fx-background-radius: 30"
+    }
+
+    private val mainMenuButton = Button(
+        posX = 870, posY = 850,
+        width = 180, height = 50,
+        text = "Main Menu",
+        font = Font(size = 25, color = Color.WHITE, family = DEFAULT_FONT_MEDIUM),
+        alignment = Alignment.CENTER,
+        visual = Visual.EMPTY
+    ).apply {
+        componentStyle = "-fx-background-color: rgb($DEFAULT_BLUE_STRING);-fx-background-radius: 30"
+    }
+
+    init {
+        opacity = 1.0
+        background = ColorVisual(247,247,247)
+        addComponents(
+            logoPane, scoreboardPane, exitButton, mainMenuButton
+        )
+    }
 }
