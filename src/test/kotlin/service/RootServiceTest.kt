532ef9209e8b633df485a4145c2c623216f74fca
oezdem05 <83602804+oezdem05@users.noreply.github.com>
2023-01-10 15:41:11 +0100

    Updated implemented suggestions
    updates #65

--- src/test/kotlin/service/RootServiceTest.kt
+++ src/test/kotlin/service/RootServiceTest.kt
@@ -15,20 +15,8 @@ class RootServiceTest {
     @Test
     fun testConstructor() {
         val rootService = RootService()
-        val playerActionService = PlayerActionService(rootService)
-        val cableCarService = CableCarService(rootService)
-        val setupService = SetupService(rootService)
-        val ioService = IOService(rootService)
-        val aiService = AIService(rootService)
-        val networkService = NetworkService(rootService)
 
         assertEquals(rootService, rootService)
-        assertEquals(playerActionService, rootService.playerActionService)
-        assertEquals(cableCarService, rootService.cableCarService)
-        assertEquals(setupService, rootService.setupService)
-        assertEquals(ioService, rootService.ioService)
-        assertEquals(aiService, rootService.aIService)
-        assertEquals(networkService, rootService.networkService)
         assertNotNull(rootService)
     }
 
