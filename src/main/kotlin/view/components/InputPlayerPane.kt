508e78f88f783fc1c0128e09ba17fcd99e73e08b
fabian.timmermann <fabian.timmermann@tu-dortmund.de>
2023-01-12 22:40:49 +0100

    Finishing (Fixing & KDoc) of InputPlayerPane

--- src/main/kotlin/view/components/InputPlayerPane.kt
+++ src/main/kotlin/view/components/InputPlayerPane.kt
@@ -12,48 +12,35 @@ import view.DEFAULT_FONT_BOLD
 import view.DEFAULT_FONT_MEDIUM
 import java.awt.Color
 
-/** A Pane which contains a "playerField" in the lobbyScene. Width: 755, Height: 75
+/** A Pane which contains a "playerField" in the lobbyScene.
+ *
+ * Default width: 590, default height: 90
  *
  * @param posX Horizontal coordinate for this Pane. Default: 0.
  * @param posY Vertical coordinate for this Pane. Default: 0.
- * @param playerNumber The Number of the Player (e.g. 1 -> "Player1")
- * @param color The default color of the player (e.g. ColorVisual(1,1,1) )
+ * @param isMultiplayer A boolean that indicates which kind of Pane is needed
+ * @param isHost A boolean that indicates if this Pane shows the Multiplayer-Host-Name
  * */
-class InputPlayerPane(posX: Number = 0, posY: Number = 0, val playerNumber: Int, color: ColorVisual,
-                      val isMultiplayer : Boolean = false, val isHost : Boolean = false) :
-    Pane<UIComponent>(posX, posY, 0, 0) {
+class InputPlayerPane(posX: Number = 0, posY: Number = 0, private val isMultiplayer : Boolean = false,
+                      private val isHost : Boolean = false) : Pane<UIComponent>(posX, posY, 590, 90) {
 
         private val playerTypes = listOf(PlayerType.HUMAN, PlayerType.AI_EASY, PlayerType.AI_HARD)
         private var typeCounter = 0
 
-        private val colorLabel = Label(
-            posX = 0, posY = 42,
-            width = 75, height = 16,
-            visual = color
-        )
-
-        private val playerLabel = Label(
-            posX = 100, posY = 30,
-            width = 90, height = 40,
-            text = "Player $playerNumber:",
-            alignment = Alignment.CENTER,
-            font = Font(size = 21, color = DEFAULT_BLUE, family = DEFAULT_FONT_BOLD)
-        )
-
         private val inputBackground = Label(
-            posX = 200, posY = 15,
+            posX = 20, posY = 15,
             width = 570, height = 75
         ).apply { componentStyle = "-fx-effect: dropshadow(one-pass-box, rgba(0,0,0,0.2), 10, 0, -1, 2);" +
                     "-fx-background-radius: 10;-fx-background-color: rgba(255,255,255,1);"}
 
         private val circleLabel = Label(
-            posX = 725, posY = 42,
+            posX = 545, posY = 42,
             width = 25, height = 25
         ).apply { componentStyle = "-fx-background-radius: 100;-fx-background-color: rgba(255,255,255,1);" +
                     "-fx-border-color: rgba(5, 24, 156, 1); -fx-border-radius: 100; -fx-border-width: 4;"}
 
         private val playerName = Label(
-            posX = 230, posY = 38,
+            posX = 50, posY = 38,
             width = 300, height = 30,
             alignment = Alignment.CENTER_LEFT,
             font = Font(size = 20, color = DEFAULT_BLUE, family = DEFAULT_FONT_MEDIUM,
@@ -61,8 +48,17 @@ class InputPlayerPane(posX: Number = 0, posY: Number = 0, val playerNumber: Int,
             text = "Waiting for Player..."
         ).apply { isVisible = false }
 
+        private val nameField = TextField(
+            posX = 40, posY = 30,
+            width = 300, height = 30,
+            prompt = "Type a name ...",
+            font = Font(size = 24, family = DEFAULT_FONT_BOLD)
+        ).apply { componentStyle = "-fx-background-color: rgba(0,0,0,0); -fx-border-color: rgb(5,24,156);" +
+                " -fx-border-width: 0 0 3 0"
+                }
+
         init {
-            addAll(inputBackground, colorLabel, playerLabel, circleLabel, playerName)
+            addAll(inputBackground, circleLabel, playerName)
 
             if(isMultiplayer){
                 multiPlayerConfiguration()
@@ -71,9 +67,11 @@ class InputPlayerPane(posX: Number = 0, posY: Number = 0, val playerNumber: Int,
             }
         }
 
+
+        /** A Method for creating all & adding needed components for the Hot-Seat LobbyScene*/
         private fun singlePlayerConfiguration(){
             val playerType = Button(
-                posX = 540, posY = 33,
+                posX = 360, posY = 33,
                 width = 145, height = 31,
                 font = Font(size = 21, color = Color(233,233,236), family = DEFAULT_FONT_BOLD),
                 text = "${playerTypes[typeCounter]}"
@@ -83,22 +81,15 @@ class InputPlayerPane(posX: Number = 0, posY: Number = 0, val playerNumber: Int,
                 }
             }
 
-            val nameField = TextField(
-                posX = 220, posY = 30,
-                width = 300, height = 30,
-                prompt = "Player $playerNumber",
-                font = Font(size = 24, family = DEFAULT_FONT_BOLD)
-            ).apply { componentStyle = "-fx-background-color: rgba(0,0,0,0); -fx-border-color: rgb(5,24,156);" +
-                    " -fx-border-width: 0 0 3 0" }
-
             addAll(playerType, nameField)
         }
 
+    /** A Method for creating all needed components for the Multiplayer LobbyScene*/
         private fun multiPlayerConfiguration(){
             playerName.isVisible = true
             if(isHost){
                 val hostLabel = Label(
-                    posX = 600, posY = 36,
+                    posX = 420, posY = 36,
                     width = 87, height = 34,
                     font = Font(size = 23, color = Color(255,255,255), family = DEFAULT_FONT_BOLD),
                     text = "HOST",
@@ -107,7 +98,7 @@ class InputPlayerPane(posX: Number = 0, posY: Number = 0, val playerNumber: Int,
                 addAll(hostLabel)
             }else{
                 val kickButton = Button(
-                    posX = 187, posY = 8,
+                    posX = 7, posY = 8,
                     width = 35, height = 20
                 ).apply { componentStyle = "-fx-background-radius: 100;-fx-background-color: rgba(255,255,255,1);" +
                         "-fx-border-color: rgba(212, 41, 38, 1); -fx-border-radius: 100; -fx-border-width: 4;" +
@@ -116,7 +107,7 @@ class InputPlayerPane(posX: Number = 0, posY: Number = 0, val playerNumber: Int,
                         scale = 0.8
                 }
                 val crossLabel = Label(
-                    posX = 187, posY = 12,
+                    posX = 7, posY = 12,
                     width = 35, height = 20,
                     alignment = Alignment.CENTER,
                     font = Font(size = 22, fontWeight = Font.FontWeight.BOLD, color = Color(255,255,255)),
@@ -127,9 +118,15 @@ class InputPlayerPane(posX: Number = 0, posY: Number = 0, val playerNumber: Int,
             }
         }
 
+        /** A Method to change the name of the displayed player name*/
         fun changePlayerName(name : String){
             if(isMultiplayer){
                 playerName.text = name
             }
+        }
+
+        /** A Method to get the String that players are typing in the textfield in Hot-Seat-Mode.*/
+        fun getTextFieldInput() : String{
+            return nameField.text
         }
     }
