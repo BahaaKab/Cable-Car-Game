2aade59444d5e7a345dcd92f779b94c326b250c0
fabian.timmermann <fabian.timmermann@tu-dortmund.de>
2023-01-12 19:27:12 +0100

    Adding Multiplayer-Mode to LobbyScene

--- src/main/kotlin/view/LobbyScene.kt
+++ src/main/kotlin/view/LobbyScene.kt
@@ -15,31 +15,26 @@ import java.awt.Color
 import javax.imageio.ImageIO
 
 
-/** LobbyScene shows the settings- & Player-Lobby in HotSeat-Mode.
+/** LobbyScene shows the settings- & Player-Lobby in HotSeat- or Multiplayer-Mode.
  *
- * @param rootService A reference to the administration of the Game-State*/
-class LobbyScene(private val rootService: RootService) : MenuScene(1920, 1080), Refreshable {
+ * @param rootService A reference to the administration of the Game-State
+ * @param isMultiplayer A boolean that defines which game mode the LobbyScene shows
+ * @param hostName The name of the hostPlayer if the scene shows a Multiplayer-Lobby*/
+class LobbyScene(private val rootService: RootService, private val isMultiplayer : Boolean = false,
+                 val hostName : String = "") : MenuScene(1920, 1080), Refreshable {
 
     private val cableCarLogo = CableCarLogo(810,50).apply { scale = 1.1 }
 
-    private val menuButton = Button(
-        posX = 585, posY = 210,
-        width = 110, height = 30,
-        text = "Menu",
-        font = Font(size = 21, color = Color.WHITE, family = DEFAULT_FONT_BOLD,
-                fontWeight = Font.FontWeight.BOLD),
-        alignment = Alignment.CENTER_RIGHT,
-        visual = ColorVisual(249, 249, 250)
-    ).apply { componentStyle = "-fx-background-color: rgba(233,233,236,1);-fx-background-radius: 100" }
-
     private val backArrow = Label(
-        posX = 594, posY = 215,
+        posX = 644, posY = 215,
         width = 30, height = 30,
         visual = ImageVisual(ImageIO.read(LobbyScene::class.java.getResource("/arrow_back.jpg")))
     )
 
+    private val backButton = backButton()
+
     private val playerOrderButton = Button(
-        posX = 720, posY = 210,
+        posX = 770, posY = 210,
         width = 264, height = 30,
         text = "Shuffle Player Order",
         font = Font(size = 21, color = Color.WHITE, family = DEFAULT_FONT_BOLD,
@@ -49,13 +44,13 @@ class LobbyScene(private val rootService: RootService) : MenuScene(1920, 1080), 
     ).apply { componentStyle = "-fx-background-color: rgba(233,233,236,1);-fx-background-radius: 100" }
 
     private val cubePicture = Label(
-        posX = 731, posY = 216,
+        posX = 781, posY = 216,
         width = 28, height = 28,
         visual = ImageVisual(ImageIO.read(LobbyScene::class.java.getResource("/cube.jpg")))
     )
 
     private val tileRotationButton = Button(
-        posX = 1014, posY = 210,
+        posX = 1064, posY = 210,
         width = 215, height = 30,
         text = "Enable Rotation",
         font = Font(size = 21, color = Color.WHITE, family = DEFAULT_FONT_BOLD,
@@ -65,27 +60,17 @@ class LobbyScene(private val rootService: RootService) : MenuScene(1920, 1080), 
     ).apply { componentStyle = "-fx-background-color: rgba(233,233,236,1);-fx-background-radius: 100" }
 
     private val refreshArrow = Label(
-        posX = 1024, posY = 216,
+        posX = 1074, posY = 216,
         width = 28, height = 28,
         visual = ImageVisual(ImageIO.read(LobbyScene::class.java.getResource("/arrow_refresh.jpg")))
     )
 
-    private val player2Display = NumberOfPlayersPane(555, 290, 2)
-    private val player3Display = NumberOfPlayersPane(680, 290, 3)
-    private val player4Display = NumberOfPlayersPane(805, 290, 4)
-    private val player5Display = NumberOfPlayersPane(930, 290, 5)
-    private val player6Display = NumberOfPlayersPane(1055, 290, 6)
+    private val playerDisplay = playerDisplay()
 
-    // private val playerXDisplay = NumberOfPlayersPane( 555 + 125 * (X-1), 290, X)
+    private val colors = listOf(DEFAULT_YELLOW_COLOR, DEFAULT_BLUE_COLOR, DEFAULT_RED_COLOR,
+                                DEFAULT_GREEN_COLOR, DEFAULT_PURPLE_COLOR, DEFAULT_BLACK_COLOR)
 
-    private val player1 = InputPlayerPane(575, 375 , 1, DEFAULT_YELLOW_COLOR)
-    private val player2 = InputPlayerPane(575, 465, 2, DEFAULT_BLUE_COLOR)
-    private val player3 = InputPlayerPane(575, 555, 3, DEFAULT_RED_COLOR)
-    private val player4 = InputPlayerPane(575, 645, 4, DEFAULT_GREEN_COLOR)
-    private val player5 = InputPlayerPane(575, 735, 5, DEFAULT_PURPLE_COLOR)
-    private val player6 = InputPlayerPane(575, 825, 6, DEFAULT_BLACK_COLOR)
-
-    // private val playerX = InputPlayerPane( 575, 375 + 90 * (X-1), X, ColorX)
+    private val playerInputs = playerInput()
 
     private val startButton = Button(
         posX = 860, 950,
@@ -101,10 +86,106 @@ class LobbyScene(private val rootService: RootService) : MenuScene(1920, 1080), 
         opacity = 1.0
         background = ColorVisual(247, 247, 247)
 
-        addComponents(cableCarLogo, player2Display, player3Display, player4Display, player5Display, player6Display)
-        addComponents(menuButton, playerOrderButton, tileRotationButton, startButton)
-        addComponents(backArrow, cubePicture, refreshArrow)
-        addComponents(player1,player2,player3,player4,player5,player6)
+        addComponents(cableCarLogo , backButton, playerOrderButton, tileRotationButton, startButton,
+                      backArrow, cubePicture, refreshArrow)
+
+        if(isMultiplayer){
+            connectionLabel()
+            for (input in playerInputs) {
+                addComponents(input)
+            }
+            playerInputs[0].changePlayerName(hostName)
+        }else {
+            for (display in playerDisplay) {
+                addComponents(display)
+            }
+            for (input in playerInputs) {
+                addComponents(input)
+            }
+        }
+    }
+
+
+    private fun backButton() : Button {
+        var i = 0
+        var name = "Menu"
+        if(isMultiplayer){
+            i = 78
+            name = "Leave Lobby"
+        }
+
+        backArrow.posX = (backArrow.posX - i)
+
+        return Button(
+                posX = (635-i), posY = 210,
+                width = (110+i), height = 30,
+                text = name,
+                font = Font(size = 21, color = Color.WHITE, family = DEFAULT_FONT_BOLD,
+                    fontWeight = Font.FontWeight.BOLD),
+                alignment = Alignment.CENTER_RIGHT,
+                visual = ColorVisual(249, 249, 250)
+            ).apply { componentStyle = "-fx-background-color: rgba(233,233,236,1);-fx-background-radius: 100" }
+    }
+
+    private fun playerDisplay() : List<NumberOfPlayersPane>{
+        val mutList = mutableListOf<NumberOfPlayersPane>()
+        for(i in 2 .. 6) {
+            mutList.add(NumberOfPlayersPane( 555 + 125 * (i-2), 290, i))
+        }
+        return mutList.toList()
+    }
+
+    private fun playerInput() : List<InputPlayerPane>{
+        val mutList = mutableListOf<InputPlayerPane>()
+        mutList.add(InputPlayerPane(575, 375 , 1, colors[0], isMultiplayer, true))
+        for(i in 2 .. 6) {
+            mutList.add(InputPlayerPane(575, 375 + 90 * (i-1), i, colors[i-1], isMultiplayer))
+        }
+        return mutList.toList()
+    }
+
+    private fun connectionLabel(){
+        val backgroundLabel = Label(
+            posX = 570, posY = 270,
+            width = 780, height = 60
+            ).apply { componentStyle = "-fx-effect: dropshadow(one-pass-box, rgba(0,0,0,0.2), 10, 0, -1, 2);" +
+                "-fx-background-radius: 10;-fx-background-color: rgba(255,255,255,1);"}
+
+        val sessionID = Label(
+            posX = 590, posY = 280,
+            width = 120, height = 40,
+            alignment = Alignment.CENTER_LEFT,
+            font = Font(size = 21, color = DEFAULT_BLUE , family = DEFAULT_FONT_BOLD,
+                        fontWeight = Font.FontWeight.BOLD),
+            text = "Session ID:"
+        )
+
+        val sesIDReal = Label(
+            posX = 710, posY = 280,
+            width = 240, height = 40,
+            alignment = Alignment.CENTER_LEFT,
+            font = Font(size = 21, color = DEFAULT_BLUE , family = DEFAULT_FONT_MEDIUM),
+            text = "thisIsOurFavouriteGame"
+        )
+
+        val secretID = Label(
+            posX = 980, posY = 280,
+            width = 100, height = 40,
+            alignment = Alignment.CENTER_LEFT,
+            font = Font(size = 21, color = DEFAULT_BLUE , family = DEFAULT_FONT_BOLD,
+                        fontWeight = Font.FontWeight.BOLD),
+            text = "Secret:"
+        )
+
+        val secretReal = Label(
+            posX = 1060, posY = 280,
+            width = 270, height = 40,
+            alignment = Alignment.CENTER_LEFT,
+            font = Font(size = 21, color = DEFAULT_BLUE , family = DEFAULT_FONT_MEDIUM),
+            text = "AMIN4PRESIDENT"
+        )
+
+        addComponents(backgroundLabel, sessionID, sesIDReal, secretID, secretReal)
     }
 
     /**
