12005db321daf3fe3dd99e368adf6a651af2a4d4
fabian.timmermann <fabian.timmermann@tu-dortmund.de>
2023-01-12 19:57:41 +0100

    Create a new Pane for showing the Player-Indication

---
+++ src/main/kotlin/view/components/PlayerIndicatorPane.kt
@@ -0,0 +1,37 @@
+package view.components
+
+import tools.aqua.bgw.components.layoutviews.Pane
+import tools.aqua.bgw.components.uicomponents.Label
+import tools.aqua.bgw.components.uicomponents.UIComponent
+import tools.aqua.bgw.core.Alignment
+import tools.aqua.bgw.util.Font
+import tools.aqua.bgw.visual.ColorVisual
+import view.DEFAULT_BLUE
+import view.DEFAULT_FONT_BOLD
+
+/** A Pane that shows the indicators of a player (color & order) as labels
+ *
+ * @param playerNumber The Number of the Player (e.g. 1 -> "Player1")
+ * @param color The default color of the player (e.g. ColorVisual(1,1,1) )
+ * */
+class PlayerIndicatorPane(posX: Number = 0, posY: Number = 0, playerNumber: Int, color: ColorVisual) :
+    Pane<UIComponent>(posX, posY, 0, 0) {
+
+    private val colorLabel = Label(
+        posX = 0, posY = 42,
+        width = 75, height = 16,
+        visual = color
+    )
+
+    private val playerLabel = Label(
+        posX = 100, posY = 30,
+        width = 90, height = 40,
+        text = "Player $playerNumber:",
+        alignment = Alignment.CENTER,
+        font = Font(size = 21, color = DEFAULT_BLUE, family = DEFAULT_FONT_BOLD)
+    )
+
+    init {
+        addAll(colorLabel, playerLabel)
+    }
+}
