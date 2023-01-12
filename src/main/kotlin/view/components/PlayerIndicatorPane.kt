7fe588eeefaf82fcae2da34c0f9c25cda4abf279
fabian.timmermann <fabian.timmermann@tu-dortmund.de>
2023-01-12 22:58:44 +0100

    Updating KDoc in PlayerIndicatorPane

--- src/main/kotlin/view/components/PlayerIndicatorPane.kt
+++ src/main/kotlin/view/components/PlayerIndicatorPane.kt
@@ -11,11 +11,15 @@ import view.DEFAULT_FONT_BOLD
 
 /** A Pane that shows the indicators of a player (color & order) as labels
  *
+ * Default width: 190, default height: 70
+ *
+ * @param posX Horizontal coordinate for this Pane. Default: 0.
+ * @param posY Vertical coordinate for this Pane. Default: 0.
  * @param playerNumber The Number of the Player (e.g. 1 -> "Player1")
  * @param color The default color of the player (e.g. ColorVisual(1,1,1) )
  * */
 class PlayerIndicatorPane(posX: Number = 0, posY: Number = 0, playerNumber: Int, color: ColorVisual) :
-    Pane<UIComponent>(posX, posY, 0, 0) {
+    Pane<UIComponent>(posX, posY, width = 190, height = 70){
 
     private val colorLabel = Label(
         posX = 0, posY = 42,
