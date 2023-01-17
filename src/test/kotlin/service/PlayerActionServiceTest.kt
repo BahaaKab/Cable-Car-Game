224a1f18864c97ba49eea8ccd4ff0efb3fc7b673
oezdem05 <83602804+oezdem05@users.noreply.github.com>
2023-01-17 15:42:10 +0100

    Tested the undo function
    By testing this function i recognized a bug. For further details see: https://sopra-gitlab.cs.tu-dortmund.de/sopra22D/gruppe06/Projekt2/-/issues/80
    
    updates #59

--- src/test/kotlin/service/PlayerActionServiceTest.kt
+++ src/test/kotlin/service/PlayerActionServiceTest.kt
@@ -15,10 +15,30 @@ class PlayerActionServiceTest {
     val players = listOf<PlayerInfo>(player1, player2)
 
 
+    /**
+     * Tests the undo function
+     *
+     * First we check that the undo stack and redo stack are empty
+     * Afterwards we do a player move and check if the undo states are not empty anymore
+     * After the undo we have to test if the undo stack is empty and the redo stack is not
+     * **/
     @Test
     fun testUndo(){
-        //Diese Methode habe ich gemacht. Wir haben aber vereinbart, dass man gegentesten soll.
-        //Daher muss jemand anderes diese Methode testen
+        setup.startLocalGame(players, false, 0)
+        val game = rootService.cableCar.currentState
+        val history = rootService.cableCar.history
+        //Tests if the initialization works
+        assertEquals(history.redoStates.size, 0)
+        assertEquals(history.undoStates.size, 0)
+        //Executes a player action
+        rootService.playerActionService.placeTile(1,3)
+        assertNotNull(game.board[1][3])
+        rootService.cableCarService.nextTurn()
+        //Tests the undo function
+        assertEquals(history.undoStates.size,1)
+        rootService.playerActionService.undo()
+        assertNull(game.board[1][3])
+        assertEquals(history.redoStates.size, 1)
     }
 
     @Test
