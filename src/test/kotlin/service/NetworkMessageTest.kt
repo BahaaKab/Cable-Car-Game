72631daac95382b1c345e800b9f98457e1cdc40f
philip <popien.philip@gmail.com>
2023-01-20 21:50:04 +0100

    Refactor Network Tests

---
+++ src/test/kotlin/service/NetworkMessageTest.kt
@@ -0,0 +1,13 @@
+package service
+
+import entity.Color
+import entity.PLAYER_ORDER_COLORS
+import entity.PlayerInfo
+import entity.PlayerType
+import tools.aqua.bgw.net.common.notification.PlayerJoinedNotification
+import tools.aqua.bgw.net.common.notification.PlayerLeftNotification
+import tools.aqua.bgw.net.common.response.*
+
+class NetworkMessageTest: NetworkTest() {
+
+}
