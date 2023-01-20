de9e8362a281659b03af6a07ea5fec51b34187d3
philip <popien.philip@gmail.com>
2023-01-20 11:18:18 +0100

    Separate NetworkRefreshable class to its own file

--- src/test/kotlin/service/NetworkTest.kt
+++ src/test/kotlin/service/NetworkTest.kt
@@ -4,102 +4,14 @@ import entity.Color
 import entity.PLAYER_ORDER_COLORS
 import entity.PlayerInfo
 import entity.PlayerType
-import tools.aqua.bgw.net.common.notification.Notification
 import tools.aqua.bgw.net.common.notification.PlayerJoinedNotification
 import tools.aqua.bgw.net.common.notification.PlayerLeftNotification
 import tools.aqua.bgw.net.common.response.*
 
-import view.Refreshable
 import kotlin.random.Random
 import kotlin.random.nextUInt
 import kotlin.test.*
 
-
-/**
- * A class to handle network [Response] and [Notification]
- */
-class NetworkRefreshable() : Refreshable {
-    /**
-     * A response or null. May be either a [CreateGameResponse] or a [JoinGameResponse]
-     */
-    var response: Response? = null
-
-    /**
-     * A notification or null. May be either a [PlayerJoinedNotification] or a [PlayerLeftNotification]
-     */
-    var notification : Notification? = null
-
-    /**
-     * Set the [NetworkRefreshable.response] property
-     *
-     * @param response The response
-     */
-    override fun refreshAfterNetworkResponse(response: Response) {
-        this.response = response
-    }
-
-    /**
-     * Set the [NetworkRefreshable.notification] property
-     *
-     * @param notification The notification
-     */
-    override fun refreshAfterNetworkNotification(notification: Notification) {
-        this.notification = notification
-    }
-
-    /**
-     * Whether a connection was successful within a timeout duration.
-     * A connection is successful when the response was returning either a [CreateGameResponseStatus.SUCCESS] or a
-     * [JoinGameResponseStatus.SUCCESS].
-     *
-     * @param timeoutInMillis The timeout duration in milliseconds
-     *
-     * @return Whether the connection was successful
-     */
-    fun responseSuccessWithin(timeoutInMillis: Int) : Boolean {
-        responseWithinOrNull(timeoutInMillis)
-        return (response is JoinGameResponse && (response as JoinGameResponse).status == JoinGameResponseStatus.SUCCESS) ||
-                (response is CreateGameResponse && (response as CreateGameResponse).status == CreateGameResponseStatus.SUCCESS)
-    }
-
-    /**
-     * Return a notification with in a timeout duration or null otherwise.
-     *
-     * @param timeoutInMillis The timeout duration in milliseconds
-     *
-     * @return A notification or null, when the timeout was exceeded
-     */
-    fun notificationWithinOrNull(timeoutInMillis: Int) : Notification? {
-        var counter = timeoutInMillis
-        while(notification == null) {
-            Thread.sleep(5)
-            if (counter <= 0) {
-                return null
-            }
-            counter -= 5
-        }
-        return notification
-    }
-
-    /**
-     * Return a response with in a timeout duration or null otherwise.
-     *
-     * @param timeoutInMillis The timeout duration in milliseconds
-     *
-     * @return A response or null, when the timeout was exceeded
-     */
-    private fun responseWithinOrNull(timeoutInMillis: Int) : Response? {
-        var counter = timeoutInMillis
-        while(response == null) {
-            Thread.sleep(5)
-            if (counter <= 0) {
-                return null
-            }
-            counter -= 5
-        }
-        return response
-    }
-}
 
 /**
  * Tests for the [NetworkService] and the [CableCarNetworkClient].
