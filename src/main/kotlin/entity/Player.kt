7facc3c7bdbe84a32bf577d9d238bd46cb2cb4d0
Erik Schmidt <erik.schmidt@tu-dortmund.de>
2023-01-22 13:31:07 +0100

    Rename Player.deepCopy to Player.copy

--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -32,7 +32,7 @@ data class Player(val playerType : PlayerType, val color : Color, val name : Str
      * Function that returns a deep-copy of a [Player]
      * @return deep-copy of [Player]
      */
-    fun deepCopy() : Player {
+    fun copy() : Player {
         return Player(playerType, color, name, stationTiles, isNetworkPlayer).also {
             it.score = score
             it.handTile = handTile?.deepCopy()
