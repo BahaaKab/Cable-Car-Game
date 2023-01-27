9debe73684d41ba51d979d6a9054131508ec2a9e
philip <popien.philip@gmail.com>
2023-01-27 10:10:01 +0100

    Add time tracking to benchmark tests

--- src/test/kotlin/service/AIServiceTest.kt
+++ src/test/kotlin/service/AIServiceTest.kt
@@ -1,7 +1,10 @@
 package service
 
 import entity.*
+import java.lang.Long.max
+import kotlin.system.measureTimeMillis
 import kotlin.test.*
+
 
 class AIServiceTest {
     @Test
@@ -14,22 +17,29 @@ class AIServiceTest {
 
     fun benchmarkHardAIWithoutRotation(numberOfRuns: Int, numberOfEasyEnemies: Int) {
         require(numberOfEasyEnemies in 1..5)
-
         val hardAI = PlayerInfo("Hard AI", PlayerType.AI_HARD, Color.YELLOW, false)
-        var hardAIWins = 0f
-
         val enemies = List(numberOfEasyEnemies) { i ->
             PlayerInfo("Easy AI$i", PlayerType.AI_EASY, PLAYER_ORDER_COLORS[i], false)
         }
-
         val playerInfos = listOf(hardAI) + enemies
 
+        var hardAIWins = 0f
+        var totalActions = 0
+        var totalTime = 0L
+        var maxTime = 0L
         repeat(numberOfRuns) {
             val rootService = RootService()
             rootService.setupService.startLocalGame(playerInfos.shuffled(), false, 0)
 
             while (rootService.cableCar.currentState.drawPile.isNotEmpty()) {
-                rootService.aIService.makeAIMove()
+                if (rootService.cableCar.currentState.activePlayer.name == hardAI.name) {
+                    totalActions ++
+                    val elapsedTime = measureTimeMillis { rootService.aIService.makeAIMove() }
+                    totalTime += elapsedTime
+                    maxTime = max(maxTime, elapsedTime)
+                } else {
+                    rootService.aIService.makeAIMove()
+                }
             }
 
             if (rootService.cableCar.currentState.players.sortedByDescending { it.score }.first().name == hardAI.name) {
@@ -40,7 +50,8 @@ class AIServiceTest {
         val winRate = hardAIWins / numberOfRuns
         val average = 1f / playerInfos.size
         val diff = winRate - average
-        println("Win rate after $numberOfRuns games against $numberOfEasyEnemies enemies: ${hardAIWins / numberOfRuns}%. Diff to average: $diff")
+        println("Win rate after $numberOfRuns games against $numberOfEasyEnemies enemies: ${hardAIWins / numberOfRuns}%. Diff to average: $diff.")
+        println("Average elapsed Time: ${totalTime / totalActions} ms. Max time elapsed for a single action: ${maxTime} ms.")
     }
 
 
