492be052865f00e319387bddae2b19253b63e140
Erik Schmidt <erik.schmidt@tu-dortmund.de>
2023-01-29 19:56:23 +0100

    Fix score not fully shown when over 100 points.

--- src/main/kotlin/view/components/OtherPlayerPane.kt
+++ src/main/kotlin/view/components/OtherPlayerPane.kt
@@ -27,20 +27,20 @@ class OtherPlayerPane(posX: Number = 0, posY: Number = 0) :
 
     val playerColorLabel = Label(
         posX = 0, posY = 122,
-        width = 30, height = 6
+        width = 23, height = 6
     )
 
     val playerNameLabel = Label(
-        posX = 40, posY = 116,
-        width = 60, height = 12,
+        posX = 30, posY = 116,
+        width = 70, height = 12,
         text = "Player 2",
         font = Font(size = 14, color = java.awt.Color(5, 24, 156), family = DEFAULT_FONT_MEDIUM),
         alignment = Alignment.CENTER_LEFT
     )
 
     val playerScoreLabel = Label(
-        posX = 40, posY = 137,
-        width = 60, height = 12,
+        posX = 30, posY = 137,
+        width = 70, height = 12,
         text = "Score: 0",
         font = Font(size = 14, color = java.awt.Color(5, 24, 156), family = DEFAULT_FONT_MEDIUM),
         alignment = Alignment.CENTER_LEFT
