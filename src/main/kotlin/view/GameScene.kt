6585079c197f52bafa2841e37a506af2069122d7
philip <popien.philip@gmail.com>
2023-01-30 11:43:31 +0100

    Simplify condition

--- src/main/kotlin/view/GameScene.kt
+++ src/main/kotlin/view/GameScene.kt
@@ -97,7 +97,7 @@ class GameScene(private val rootService: RootService) : BoardGameScene(1920, 108
             for (j in 1..8) {
 
                 // We don't want to place empty tiles where the power stations are
-                if ((i == 4 || i == 5) && (j == 4 || j == 5)) continue
+                if (i in 4..5 && j in 4..5) continue
 
                 set(columnIndex = i, rowIndex = j, component = emptyTilesCardViews[i - 1][j - 1])
             }
